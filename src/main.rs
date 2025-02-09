fbc7c7dd6a278de068dae8d5492a4d5dc1c2fc5c
standarddeviant <dave.crist@gmail.com>
2025-02-09 09:47:15 -0500

    default log config + run python unbuffered w/ -u

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,41 @@ use duct::cmd;
 use log::{debug, error, info};
 use std::io::Read;
 
+use env_logger::Builder;
+use log::LevelFilter;
+use std::io::Write;
+
 fn main() {
-    env_logger::init();
+    let mut builder = Builder::new();
+    builder
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{}: {}: {}",
+                buf.timestamp(),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter_level(LevelFilter::Info)
+        .init();
+
     let mut buf: Vec<u8> = vec![];
     let mut single_byte = [0u8];
 
-    let mut reader = cmd!("python", "test.py").reader().unwrap();
+    let mut reader = cmd!("python", "-u", "test.py").reader().unwrap();
     info!("Started command reader:\n{reader:?}");
 
+    let wait_until_str = "hello to stdout";
+
+    info!("Waiting for -->{wait_until_str}<--");
     loop {
         match reader.read(&mut single_byte) {
             Ok(_good) => {
                 buf.push(single_byte[0]);
                 if let Ok(chks) = std::str::from_utf8(&buf) {
                     debug!("chks = {chks}");
-                    if chks.contains("hello to stdout") {
+                    if chks.contains(wait_until_str) {
                         info!("Breaking from wait-start loop");
                         break;
                     }
