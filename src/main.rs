8d4024cf7d79bdc9467f227d40a0c2853c8ee981
standarddeviant <dave.crist@gmail.com>
2025-02-10 14:30:19 -0500

    working by using a real file for rust->python
    non-ideal, but functional...

--- src/main.rs
+++ src/main.rs
@@ -18,29 +18,47 @@ fn main() {
                 record.args()
             )
         })
-        .filter_level(LevelFilter::Info)
+        .filter_level(LevelFilter::Debug)
         .init();
 
     let mut buf: Vec<u8> = vec![];
     let mut single_byte = [0u8];
 
-    let mut reader = cmd!("python", "-u", "test.py").reader().unwrap();
+    let mut fake_stdin_wr = std::fs::File::create("./fake_stdin").unwrap();
+    let fake_stdin_rd = std::fs::File::open("./fake_stdin").unwrap();
+    let mut reader = cmd!("python3", "-u", "test.py")
+        .stdin_file(fake_stdin_rd)
+        .stderr_to_stdout()
+        .reader()
+        .unwrap();
+
     info!("Started command reader:\n{reader:?}");
 
     let wait_until_str = "hello to stdout";
 
     info!("Waiting for -->{wait_until_str}<--");
+    let mut idle_count = 0;
     loop {
         match reader.read(&mut single_byte) {
             Ok(_good) => {
-                buf.push(single_byte[0]);
-                if let Ok(chks) = std::str::from_utf8(&buf) {
-                    debug!("chks = {chks}");
-                    if chks.contains(wait_until_str) {
-                        info!("Breaking from wait-start loop");
-                        break;
+                if _good > 0 {
+                    buf.push(single_byte[0]);
+                    if let Ok(chks) = std::str::from_utf8(&buf) {
+                        debug!("chks = {chks}");
+                        if chks.contains(wait_until_str) {
+                            info!("Breaking from wait-start loop");
+                            break;
+                        }
                     }
                 }
+                // if _good == 0 {
+                //     std::thread::sleep(std::time::Duration::from_millis(20));
+                //     idle_count += 1;
+                //     if idle_count > 100 {
+                //         break;
+                //         //
+                //     }
+                // }
             }
             Err(_bad) => {
                 error!("{_bad}");
@@ -48,7 +66,33 @@ fn main() {
         }
     }
 
+    info!("sleeping 2s on rust side (fake delay)");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    let tmpv = vec![0];
+    match fake_stdin_wr.write(tmpv.as_slice()) {
+        Ok(_good) => {
+            info!("Wrote {_good} bytes to child process");
+        }
+        Err(_bad) => {
+            info!("_bad = {_bad}");
+        }
+    }
+    fake_stdin_wr.flush();
+
+    info!("starting loop");
     loop {
+        match reader.read(&mut single_byte) {
+            Ok(num_bytes) => {
+                if num_bytes > 0 {
+                    buf.push(single_byte[0]);
+                    continue;
+                }
+            }
+            Err(_bad) => {
+                error!("hmmm... {_bad}");
+            }
+        }
         match reader.try_wait() {
             Ok(Some(_good)) => {
                 info!("Breaking from wait-finish, output = {_good:?}");
@@ -64,22 +108,10 @@ fn main() {
             }
         }
     }
-    //     match reader.read(&mut single_byte) {
-    //         Ok(_good) => {
-    //             buf.push(single_byte[0]);
-    //             if let Ok(chks) = std::str::from_utf8(&buf) {
-    //                 debug!("chks = {chks}");
-    //                 if chks.contains("hello to stdout") {
-    //                     info!("Breaking from initial loop");
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //         Err(_bad) => {
-    //             error!("{_bad}");
-    //         }
-    //     }
-    // }
+
+    if let Ok(chks) = std::str::from_utf8(&buf) {
+        info!("final state of (stdout) buf = \n{chks}");
+    }
 
     info!("Quitting time...")
 }
--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 
+import logging
 import sys
 import time
 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+
 time.sleep(2.0)
+logging.info("hello to stdout AFTER init-sleep")
 
-print("hello to stdout")
+while True:
+    tmp = sys.stdin.read()
+    if len(tmp) > 0:
+        break;
+logging.info("hello to stdout AFTER non-zero-length stdin.read()")
 
 time.sleep(2.0)
+logging.info("hello to stdout AFTER post-read-sleep")
 
-print("hello again!")
-
+sys.stdout.flush()
 sys.exit(0)
 
